a06ac9bfad535e64e698af196c0b3dac1cb44c9c
Petr Warzel <warzel@cngroup.dk>
2019-02-11 20:08:41 +0100

    basic structure - components, routing, service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NotesDetailComponent } from './notes-detail/notes-detail.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    NotesDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
